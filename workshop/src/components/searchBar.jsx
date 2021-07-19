27e5346ce50eb02e72546b52b5e9fb1135791611
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-07-19 23:29:35 +0900

    added files

---
+++ workshop/src/components/searchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <div className="form-search">
+      <input type="text" placeholder="Search GIF"/>
+    </div>
+  )
+};
+
+export default SearchBar;
